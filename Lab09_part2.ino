32ad79d3ced1e71ddc159a3c119531c5cdc3abf1
05050821 <37134119+05050821@users.noreply.github.com>
2018-05-30 15:20:53 +0800

    Add files via upload

---
+++ Lab09_part2.ino
@@ -0,0 +1,16 @@
+#include <IRremote.h>
+const int buttonPin = 6;
+int buttonState = 0;
+IRsend irsend; // IRRemote限定使用數位腳位3
+
+void setup()
+{
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+  if(buttonState == HIGH){
+    irsend.sendNEC(0x4FB48B7, 32);
+    }
+  }
